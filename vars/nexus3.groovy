850a930d7b3537036998373cd045eda1639c9f62
Steve Maring <steve.maring@hitachivantara.com>
2018-10-10 11:10:59 -0400

    added grab

--- vars/nexus3.groovy
+++ vars/nexus3.groovy
@@ -2,6 +2,7 @@
 @Grab('com.hitachivantara.repo:nexus3:1.0.2')
 import com.hitachivantara.repo.nexus3.Nexus3RepoManager;
 
+@Grab('org.sonatype.nexus:nexus-repository:3.13.0-01')
 @Grab('org.sonatype.nexus.plugins:nexus-repository-maven:3.13.0-01')
 import org.sonatype.nexus.repository.maven.VersionPolicy;
 
